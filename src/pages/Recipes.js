132572ded7358564c56f9d2ab0dde61bb729fcd1
Rajeev Joshi <rajeevjoshi91@gmail.com>
2022-04-10 21:18:45 +0530

    previous search items and search box

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,7 +1,44 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 const Recipes = () => {
-  return <div>Recipes</div>;
+  const [searches, setSearches] = useState([
+    "pizza",
+    "burger",
+    "cookies",
+    "juice",
+    "biriyani",
+    "salad",
+    "ice cream",
+    "lasagna",
+    "pudding",
+    "soup",
+  ]);
+
+  return (
+    <div>
+      <div className="previous-searches section">
+        <h2>Previous Searches</h2>
+        <div className="previous-searches-container">
+          {searches.map((search, index) => (
+            <div
+              style={{ animationDelay: index * 0.1 + "s" }}
+              className="search-item"
+              key={index}
+            >
+              {search}
+            </div>
+          ))}
+        </div>
+        <div className="search-box">
+          <input type="text" placeholder="search for a recipe..." />
+          <button className="btn">
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Recipes;
