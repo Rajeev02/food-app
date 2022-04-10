e3cc263aa9d86842f0c19c5cdc5133a3796cae5e
Rajeev Joshi <rajeevjoshi91@gmail.com>
2022-04-10 13:22:15 +0530

    Hero section complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <div className="container main">
+        <HeroSection />
+      </div>
     </div>
   );
 }
