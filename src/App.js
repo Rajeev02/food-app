ab4fd7e07fb9cc995352610067420f1380f9d5a5
Rajeev Joshi <rajeevjoshi91@gmail.com>
2022-04-10 14:10:30 +0530

    improve skil srction complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HeroSection from "./components/HeroSection";
+import ImproveSkills from "./components/ImproveSkills";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <div className="container main">
         <HeroSection />
+        <ImproveSkills />
       </div>
     </div>
   );
