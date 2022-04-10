1c9e21f4be65f4306fdc9d37d251628f4226d5a1
Rajeev Joshi <rajeevjoshi91@gmail.com>
2022-04-10 09:41:12 +0530

    nav bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+function Navbar() {
+  const [showSidebar, setShowSidebar] = useState(true);
+  return (
+    <>
+      <div className="navbar container">
+        <a href="#!" className="logo">
+          F<span>oo</span>diesHub
+        </a>
+        <div className="nav-links">
+          <a href="#!">Home</a>
+          <a href="#!">Recipes</a>
+          <a href="#!">Settings</a>
+        </div>
+        <div
+          onClick={() => {
+            setShowSidebar(!showSidebar);
+          }}
+          className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}
+        >
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
