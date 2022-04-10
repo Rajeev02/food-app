2c842a44e89c309e89b0661153e86c485115aa5a
Rajeev Joshi <rajeevjoshi91@gmail.com>
2022-04-10 21:22:55 +0530

    update previous search

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,43 +1,11 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-const Recipes = () => {
-  const [searches, setSearches] = useState([
-    "pizza",
-    "burger",
-    "cookies",
-    "juice",
-    "biriyani",
-    "salad",
-    "ice cream",
-    "lasagna",
-    "pudding",
-    "soup",
-  ]);
+import React from "react";
 
+import PreviousSearches from "../components/PreviousSearches";
+const Recipes = () => {
   return (
-    <div>
-      <div className="previous-searches section">
-        <h2>Previous Searches</h2>
-        <div className="previous-searches-container">
-          {searches.map((search, index) => (
-            <div
-              style={{ animationDelay: index * 0.1 + "s" }}
-              className="search-item"
-              key={index}
-            >
-              {search}
-            </div>
-          ))}
-        </div>
-        <div className="search-box">
-          <input type="text" placeholder="search for a recipe..." />
-          <button className="btn">
-            <FontAwesomeIcon icon={faSearch} />
-          </button>
-        </div>
-      </div>
-    </div>
+    <>
+      <PreviousSearches />
+    </>
   );
 };
 
